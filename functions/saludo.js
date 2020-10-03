98d775bf5d39d51e79a734b431ab9dd13810ae9c
gilpgcl <54386892+gilpgcl@users.noreply.github.com>
2020-10-03 13:01:42 -0500

    Update saludo.js

--- functions/saludo.js
+++ functions/saludo.js
@@ -1,7 +1,18 @@
+class ImplCtrlSaludo {
+  /** @param {string} nombre */
+  async calculaSaludo(nombre) {
+    const nombreSinEspacios = nombre.trim();
+    if (!nombreSinEspacios) {
+      throw new Error("Falta proporcionar el nombre.");
+    }
+    return `Hola ${nombreSinEspacios}.`;
+  }
+}
+const ctrlSaludo = new ImplCtrlSaludo();
 exports.handler = async event => {
   try {
     const nombre = event.queryStringParameters.nombre;
-    const saludo = `$HOla{nombre}`;
+    const saludo = await ctrlSaludo.calculaSaludo(nombre);
     return {
       statusCode: 200,
       body: saludo
