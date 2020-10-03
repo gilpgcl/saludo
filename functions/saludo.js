c71c8ffed109922a85fca5f3a4ed6f5c4d0a0726
gilpgcl <54386892+gilpgcl@users.noreply.github.com>
2020-10-03 13:16:47 -0500

    Update saludo.js

--- functions/saludo.js
+++ functions/saludo.js
@@ -4,12 +4,15 @@ const ctrlSaludo = new ImplCtrlSaludo();
 exports.handler = async event => {
   try {
     const nombre = event.queryStringParameters.nombre;
+    console.log("Entra con", nombre);
     const saludo = await ctrlSaludo.calculaSaludo(nombre);
+    console.log("Devuelve", saludo);
     return {
       statusCode: 200,
       body: saludo
     };
   } catch (error) {
+    console.error(error);
     return {
       statusCode: 500,
       body: error.message
